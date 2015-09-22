6f3d95684d4d0907847027468844571396d03fcf
Matthew <casqouro@gmail.com>
2015-09-21 22:34:28 -0400

    Adding Resources
    - Removed the logic for drawing the player, the resources, and the game
    grid from render(), into their own functions which are now called in
    render().
    - Considering using a LinkedList rather than array to track game
    objects.
    - Simple resources added to test drawing them.

--- core/src/com/mygdx/game/CraftingSimulator.java
+++ core/src/com/mygdx/game/CraftingSimulator.java
@@ -14,6 +14,14 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 // investigate disabling continuous rendering
 // investigate game loops
 
+/*  Looping through an array to draw stuff is wasteful.  Have resources exist as
+objects which maintain their color and location.  Use a linked list to track 
+every object, with the first entry always being the player.  When generating a
+resource add it to the list.  When consuming a resource delete it from the list.
+
+Right now it's looping through and checking 100 things, 97 of which are empty!
+*/
+
 public class CraftingSimulator extends ApplicationAdapter {
 	SpriteBatch batch;   
         ShapeRenderer jack;
@@ -32,7 +40,7 @@ public class CraftingSimulator extends ApplicationAdapter {
 	public void create () {
             KeyProcessor processor = new KeyProcessor();
             Gdx.input.setInputProcessor(processor);
-            
+           
             jack = new ShapeRenderer();
             batch = new SpriteBatch();            
             fieldSizeX = 10;
@@ -45,18 +53,35 @@ public class CraftingSimulator extends ApplicationAdapter {
             heightSpacing = height / fieldSizeY;
             widthSpacing = width / fieldSizeX;
             
+            gameBoard[1][1] = 2;
+            gameBoard[3][9] = 3;
 	}
         
-	@Override
-	public void render () {            
-            Gdx.gl.glClearColor(0, 0, 0, 0);
-            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); 
-                
+        private void drawResources() {
+            jack.begin(ShapeType.Filled);
+            for (int a = 0; a < fieldSizeX; a++) {
+                for (int b = 0; b < fieldSizeY; b++) {
+                    if (gameBoard[a][b] == 2) {
+                        jack.setColor(Color.GRAY);
+                        jack.rect(a * widthSpacing, b * heightSpacing, widthSpacing, heightSpacing);
+                    }
+                    if (gameBoard[a][b] == 3) {
+                        jack.setColor(Color.BLUE);
+                        jack.rect(a * widthSpacing, b * heightSpacing, widthSpacing, heightSpacing);
+                    }
+                }
+            }
+            jack.end();
+        }
+        
+        private void drawPlayer() {
             jack.begin(ShapeType.Filled);
             jack.setColor(Color.GREEN);
             jack.rect(playerX * widthSpacing, playerY * heightSpacing, widthSpacing, heightSpacing);
             jack.end();
-            
+        }
+        
+        private void drawField() {
             jack.begin(ShapeType.Line);
             jack.setColor(Color.WHITE);
             
@@ -68,38 +93,45 @@ public class CraftingSimulator extends ApplicationAdapter {
             for (int row = 0; row < fieldSizeY; row++) {
                 jack.line(0, row * height/fieldSizeY,
                           width, row * height/fieldSizeY);
-            }            
-            
-
-            
-            if(Gdx.input.isKeyPressed(Input.Keys.W)){
+            }                                 
+            jack.end();            
+        }
+        
+	@Override
+	public void render () {            
+            Gdx.gl.glClearColor(0, 0, 0, 0);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); 
                 
-            }           
-                      
-            jack.end();
-                
+            drawResources();
+            drawPlayer();
+            drawField();
+                        
             batch.begin();
             batch.end();
 	}
         
         private class KeyProcessor extends InputAdapter {
-
+            
             @Override
             public boolean keyDown(int keycode) {
                 switch (keycode) {
                     case Keys.W:
+                    case Keys.UP:
                         if (playerY + 1 < fieldSizeY) {
                             playerY += 1; }
                         break;
                     case Keys.A:
+                    case Keys.LEFT:
                         if (playerX - 1 >= 0) {
                             playerX -= 1; }
                         break;
                     case Keys.S:
+                    case Keys.DOWN:
                         if (playerY - 1 >= 0) {
                             playerY -= 1; }          
                         break;
                     case Keys.D:
+                    case Keys.RIGHT:
                         if (playerX + 1 < fieldSizeX) {
                             playerX += 1; }
                         break;
