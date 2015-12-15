771e0ed6f17986e1c652942d17df2e1e77c6a042
Matthew <casqouro@gmail.com>
2015-12-15 03:38:09 -0200

    Collision Added
    Collision has been added.
    
    Currently working on getting non-sticky collision.  An example would be
    pressing "up" and hitting a while, but still moving left if you're
    holding "left" at he same time.

--- core/src/com/mygdx/game/Entity.java
+++ core/src/com/mygdx/game/Entity.java
@@ -1,15 +1,46 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-public class Entity {
-    public Color myColor;
-    public int locX;
-    public int locY;
+public class Entity {    
+    private TextureRegion appearance;
+    int x;
+    int y;
+    int width;
+    int height;
     
-    public void Entity(Color c, int x, int y) {
-        myColor = c;
-        locX = x;
-        locY = y;
+    // Should the data members be private, with accessor methods like:
+        // setX/setY, for changes?
+        // getBounds, for collision?
+    
+    public Entity(TextureRegion a, int xLoc, int yLoc) {
+        appearance = a;
+        
+        x = xLoc;
+        y = yLoc;
+        
+        width = appearance.getRegionWidth();
+        height = appearance.getRegionHeight();
+    }    
+    
+    public Entity(TextureRegion a, int xLoc, int yLoc, int scaleWidth, int scaleHeight) {
+        appearance = a;
+        
+        x = xLoc;
+        y = yLoc;
+        
+        width = scaleWidth;
+        height = scaleHeight;
+    }
+    
+    public void drawEntity(SpriteBatch toDraw) {
+        toDraw.draw(appearance, x, y, width, height);
+    }
+    
+    // This method doesn't seem healthy.  Something about object references
+    // without cleaning up...
+    public void setAppearance(TextureRegion newAppearance) {
+        appearance = newAppearance;
     }
 }
